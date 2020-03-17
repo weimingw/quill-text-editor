86a5e007b839e59cc8fa7d86a9d18b8a82f23db1
Weiming Wu <weiming.wu@veeva.com>
2020-03-16 17:22:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
